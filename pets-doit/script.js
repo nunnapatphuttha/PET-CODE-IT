48b3b7c005227e2df2ce71d9b5e87f6a1396b577
nunnapatphuttha <nunnapatphutthatipakorn@gmail.com>
2022-03-28 17:29:06 +0700

    first commit

---
+++ pets-doit/script.js
@@ -0,0 +1,39 @@
+//JS of responsive nav menu
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+
+menuBtn.addEventListener("click", ()=>{
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+//JS of video slide nav-btn
+const btns = document.querySelectorAll(".slider-nav-btn");
+const slides = document.querySelectorAll(".video-silde");
+const contents = document.querySelectorAll(".content");
+
+
+var slideNav = function(manual){
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    contents.forEach((content) => {
+        content.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+
+btns.forEach((btn, i)=>{
+    btn.addEventListener("click",()=>{
+        slideNav(i);
+    });
+});
