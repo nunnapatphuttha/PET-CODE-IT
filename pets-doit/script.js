60b124a84914239b2c62e86ba18f4f92ad851cae
Sitthikon <sitthikon.work@gmail.com>
2022-04-01 21:41:25 +0700

    ez

--- pets-doit/script.js
+++ pets-doit/script.js
@@ -80,3 +80,22 @@ function actionToggle(){
     var action = document.querySelector('.chat-btn');
     action.classList.toggle('active')
 }
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
